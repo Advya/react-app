f6d8297f5b18c36ef3c2c9be724f2f62e2d9ca9e
advya <50411324+Advya@users.noreply.github.com>
2021-09-27 19:21:29 +0300

    Update Header.js

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,22 +1,29 @@
-
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
-export const Header = ({title}) => {
-    const onClick = () => {
-        console.log('click');
-    }
+export const Header = ({title, onAdd, showAdd }) => {
+    const location = useLocation()
+   
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button onClick={onClick} color="teal" text='Add' />
-
-
+            {location.pathname === '/' && (
+                <Button 
+                    color={showAdd ? 'red': 'green'}
+                    text={showAdd ? 'Close' : 'Add'}
+                    onClick={onAdd} 
+                />
+            )}
         </header>
     )
 }
 
 Header.defaultProps ={
     title: 'Task Tracker'
+}
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header;

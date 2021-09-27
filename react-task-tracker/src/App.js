f0fa2ace1f2f3cf9a3de27b36fac6b5cd0b571e9
advya <50411324+Advya@users.noreply.github.com>
2021-09-27 16:53:31 +0300

    useEffect and fetchTask

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,16 +1,56 @@
+import { useState, useEffect } from 'react'
+
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTask()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  //Fetch Tasks
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:3000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+  }
+
 
   //Delete Task
-  const deleteTask = async(id) => {
+  const deleteTask = async (id) => {
     const res = await fetch(`http://localhost:3000/tasks/${id}`, {
       method: 'DELETE',
     })
     res.status === 200
-    ? setTasks(tasks.filter((task) => task.id !==id))
-    : alert('Error Deleting This Task')
+      ? setTasks(tasks.filter((task) => task.id !== id))
+      : alert('Error Deleting This Task')
   }
 
   //Toggle Reminder
@@ -39,7 +79,7 @@ const App = () => {
       <div className="App">
         <Header />
         <h1>Hello From REACT</h1>
-      </div>    
+      </div>
     </div>
   );
 }

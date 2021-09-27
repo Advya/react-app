3113886ffa0297ffd27178fb7fa86fe53a6ee07a
advya <50411324+Advya@users.noreply.github.com>
2021-09-27 16:10:51 +0300

    delete method

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,6 +1,18 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+
+  //Delete Task
+  const deleteTask = async(id) => {
+    const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+    res.status === 200
+    ? setTasks(tasks.filter((task) => task.id !==id))
+    : alert('Error Deleting This Task')
+  }
+
   return (
     <div className="container">
       <div className="App">

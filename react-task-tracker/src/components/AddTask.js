ce7d4378b0b1c3ee953c71a2269628061c4cd0af
advya <50411324+Advya@users.noreply.github.com>
2021-09-27 16:59:58 +0300

    Create AddTask.js

---
+++ react-task-tracker/src/components/AddTask.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const AddTask = ({ onAdd }) => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!text) {
+            alert('Please add a task')
+            return
+        }
+
+        onAdd({ text, day, reminder })
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
+    return (
+        
+    )
+}
+
+export default AddTask

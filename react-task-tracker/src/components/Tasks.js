49b692ac3d879c703cc7e75e515531e649e57f48
advya <50411324+Advya@users.noreply.github.com>
2021-09-27 15:42:46 +0300

    import and map tasks array

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,9 +1,12 @@
+import Task from './Task'
 
-const Tasks = () => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
-        <div>
-            
-        </div>
+        <>
+          {tasks.map((task,index) => (
+              <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+          ))}  
+        </>
     )
 }
 

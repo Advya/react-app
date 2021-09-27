db7bec7eed930a952d87c586133f528d0c90db23
advya <50411324+Advya@users.noreply.github.com>
2021-09-27 16:17:34 +0300

    Task reminder func

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -13,6 +13,27 @@ const App = () => {
     : alert('Error Deleting This Task')
   }
 
+  //Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:3000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    )
+  }
+
   return (
     <div className="container">
       <div className="App">
